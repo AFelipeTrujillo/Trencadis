54ca150aec48f5754ee2d36f9a040122439b2ed1
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-01-23 19:41:36 +0100

    Exceotion handler

--- services/recipe-service/src/Infrastructure/Delivery/Http/FastAPI/main.py
+++ services/recipe-service/src/Infrastructure/Delivery/Http/FastAPI/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
+from fastapi import status
+from Domain.Exception.DomainException import DomainException
 from Infrastructure.Delivery.Http.Security.KeycloakGuard import KeycloakGuard
 
 app = FastAPI(title="Trencadís Recipe Service", version="1.0.0")
@@ -10,4 +13,14 @@ def get_recepies(user: dict = Depends(auth_guard)):
         "message": "Authenticated access to recipes",
         "user_id": user.get("sub")
     }
-    
+
+@app.exception_handler(DomainException)
+def domain_exception_handler(request, exc: DomainException):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={
+            "error": "Domain Error",
+            "type": exc.__class__.__name__,
+            "message": str(exc)
+        },
+    )
