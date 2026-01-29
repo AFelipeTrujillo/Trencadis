e088cd6f6322e06d78a9c054d7c260133d7eadf8
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-01-29 17:40:17 +0100

    RecipeaMapper and SQLAlchemyRecipieRepository

---
+++ services/recipe-service/src/Infrastructure/Persistence/SQLAlchemy/Mappers/RecipeMapper.py
@@ -0,0 +1,45 @@
+
+from Domain.Entity.Recipe import Recipe
+from Domain.Entity.Recipe import Ingredient
+from Infrastructure.Persistence.SQLAlchemy.Models.RecipeModel import RecipeModel
+from Infrastructure.Persistence.SQLAlchemy.Models.RecipeModel import IngredientModel
+
+
+class RecipeMapper():
+
+    @staticmethod
+    def to_domain(model: RecipeModel) -> Recipe:
+        
+        ingredients = [
+            Ingredient(
+                name = ingredient.name,
+                amount = ingredient.amount,
+                unit = ingredient.unit
+            ) for ingredient in model.ingredients
+        ]
+
+        return Recipe(
+            id=model.id,
+            description=model.description,
+            owner_id=model.owner_id,
+            ingredients=ingredients
+        )
+    
+    @staticmethod
+    def to_model(entity: Recipe) -> RecipeModel:
+        
+        ingredient_model = [
+            IngredientModel(
+                name = ingredient.name,
+                amount = ingredient.amount,
+                unit = ingredient.unit
+            ) for ingredient in entity.ingredients
+        ]
+
+        return RecipeModel(
+            id = entity.id,
+            name = entity.name,
+            description = entity.description,
+            owner_id = entity.owner_id,
+            ingredients = ingredient_model,
+        )
---
+++ services/recipe-service/src/Infrastructure/Persistence/SQLAlchemy/Models/SQLAlchemyRecipeRepository.py
@@ -0,0 +1,39 @@
+
+from uuid import UUID
+from typing import List
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
+
+from Domain.Entity.Recipe import Recipe
+from Domain.Repository.RecipeRepositoryInterface import RecipeRepositoryInterface
+from Infrastructure.Persistence.SQLAlchemy.Models.RecipeModel import RecipeModel
+from Infrastructure.Persistence.SQLAlchemy.Mappers.RecipeMapper import RecipeMapper
+
+class SQLAlchemyRecipeRepository(RecipeRepositoryInterface):
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        
+
+    async def save(self, recipe: Recipe):
+        model = RecipeMapper.to_model(recipe)
+
+        await self.session.merge(model)
+
+        await self.session.flush()
+
+    async def find_by_owner(self, owner_id: UUID) -> List[Recipe]:
+        
+        query = (
+            select(RecipeModel)
+            .where(RecipeModel.owner_id == owner_id)
+            .options(selectinload(RecipeModel.ingredients))
+        )
+
+        result = await self.session.execute(query)
+
+        models = result.scalars().all()
+
+        return [RecipeMapper.to_domain(model) for model in models]
+        
